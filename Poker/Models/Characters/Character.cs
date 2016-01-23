ceff5ae7b38969c11ffd26b73d1749ea4f50333e
Veronika Dzhambazova <veronika.dzhambazova@abv.bg>
2016-01-23 18:26:28 +0200

    Add Classes

--- Poker/Models/Characters/Character.cs
+++ Poker/Models/Characters/Character.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Poker.Models
+﻿namespace Poker.Models
 {
-    class Characters
+    public class Character
     {
     }
 }
